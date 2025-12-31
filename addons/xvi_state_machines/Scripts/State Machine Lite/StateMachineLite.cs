0ae724fc439171468f8d06926c4137910a9f6aec
DragonBoyXVI <DragonBoyXVI@gmail.com>
2025-12-31 13:22:17 -0800

    RefCounted simple state machine.

---
+++ addons/xvi_state_machines/Scripts/State Machine Lite/StateMachineLite.cs
@@ -0,0 +1,37 @@
+using Godot;
+
+namespace DragonXVI.StateMachines;
+
+/// <summary>
+/// Simple match based state machine wrapper.
+/// Also emits godot signals when the "state" is changed.
+/// </summary>
+public partial class StateMachineLite : RefCounted
+{
+    /// <summary>
+    /// Emitted after the state is changed.
+    /// </summary>
+    /// <param name="state">The new state</param>
+    [Signal]
+    public delegate void StateEnteredEventHandler(int state);
+    /// <summary>
+    /// Emitted before the state is changed.
+    /// </summary>
+    /// <param name="state">The old state</param>
+    [Signal]
+    public delegate void StateLeftEventHandler(int state);
+
+    private int State;
+
+    public int GetState()
+    {
+        return State;
+    }
+
+    public void SetState( int state )
+    {
+        EmitSignal(SignalName.StateLeft, State);
+        State = state;
+        EmitSignal(SignalName.StateEntered, State);
+    }
+}
