13b45557a05fdde2acafd704274f36fa0307db6f
DragonXVI <Dragonboyxvi@gmail.com>
2026-04-02 01:15:59 -0700

    cleanup

--- node_2d.gd
+++ node_2d.gd
@@ -1,9 +1,6 @@
 extends Node2D
 
 
-@export var elem1: ElementGD;
-@export var elem2: ElementGD;
-
 
 func _test():
 	pass
@@ -14,8 +11,4 @@ func _input( event: InputEvent ) -> void:
 		if ( not event.is_pressed() ): return;
 		if ( event.is_echo() ): return;
 		if ( event.keycode != KEY_SPACE ): return;
-		if ( not elem1 or not elem2 ): return;
-		print( "Element 1: ", elem1.current_element );
-		print( "strength to Element 2: ", elem1.get_strength_against( elem2 ) );
-		print( "Element 2: ", elem2.current_element );
-		print( "Strength to Element 1: ", elem2.get_strength_against( elem1 ) );
+		pass;
