e81e2c4949cf588b37c0ad4218485a2a6c2059cb
DragonXVI <Dragonboyxvi@gmail.com>
2026-06-22 11:09:32 -0700

    added assets

--- addons/xvi_utilities/Scripts/Utility/XVIFuncs.cs
+++ addons/xvi_utilities/Scripts/Utility/XVIFuncs.cs
@@ -11,15 +11,16 @@ public static class XVIFuncs
     /// Disables all node processes that have a disable function.
     /// Such as process, physics process, input processes, etc.
     /// Useful for tool nodes, be sure to call this in _Ready rather than the constructor.
+    /// This effect can be reversed by using this again with "true" as the second arg.
     /// </summary>
     /// <param name="node">The node to disable.</param>
-    public static void DisableNodeProcesses(Node node)
+    public static void DisableNodeProcesses(Node node, bool enabled = false)
 	{
-		node.SetProcess(false);
-		node.SetPhysicsProcess(false);
-		node.SetProcessInput(false);
-		node.SetProcessShortcutInput(false);
-		node.SetProcessUnhandledInput(false);
-		node.SetProcessUnhandledKeyInput(false);
+		node.SetProcess(enabled);
+		node.SetPhysicsProcess(enabled);
+		node.SetProcessInput(enabled);
+		node.SetProcessShortcutInput(enabled);
+		node.SetProcessUnhandledInput(enabled);
+		node.SetProcessUnhandledKeyInput(enabled);
 	}
 }
